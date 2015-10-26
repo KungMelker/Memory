3a9275c3492ed66af1ae7febb4b27b11985573b4
JohDav <johan.davidson@yh.nackademin.se>
2015-10-26 13:18:03 +0100

    Bild

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,38 +1,36 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
 		// A simple memory game
-		//shgsjöoskfäplsdf
-		//asdas
-		
-		
-			GridPane root = new GridPane();
 
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			
-			Label label = new Label("MEMORY");
-			root.setAlignment(Pos.CENTER);
-			root.addRow(0, label);
-			
-			primaryStage.show();
-			primaryStage.setTitle("Memory v0.1");
-			
-			//random comment from EseBring3n
-	}
+		GridPane root = new GridPane();
 		
+		Image cardImage = new Image("http://mobile-visuals.com/icon.png");
+		ImageView imageView = new ImageView(cardImage);
+		
+		Scene scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+
+		root.setAlignment(Pos.CENTER);
+		root.addRow(0, imageView);
+
+		primaryStage.show();
+		primaryStage.setTitle("Memory v0.1");
+
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
