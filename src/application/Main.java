1bb4f57243dd0b16eb18e74eeafdc09ca68b3815
JohDav <johan.davidson@yh.nackademin.se>
2015-10-26 11:29:03 +0100

    Ändrade till GridPane

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,21 +3,19 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
+		
+			GridPane root = new GridPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		
 	}
 	
 	public static void main(String[] args) {
