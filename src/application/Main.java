49b0000f8c5f760de519e557b2c0fd1fa177c531
JohDav <johan.davidson@yh.nackademin.se>
2015-10-27 10:57:58 +0100

    top,center,right,bottom

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,18 +4,41 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
 		BorderPane root = new BorderPane();
+		Scene scene = new Scene(root, 400, 400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);		
 		
 		
+		// topBox
+		HBox topBox = new HBox(5);
+		topBox.setAlignment(Pos.CENTER);
+		Label titel = new Label("Memory");
+		titel.setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 25));
+		topBox.getChildren().add(titel);
+
+		// rightBox
+		VBox rightBox = new VBox(5);
+		rightBox.setAlignment(Pos.CENTER);
+
+		// centerBox
+		GridPane centerBox = new GridPane();
+		centerBox.setAlignment(Pos.CENTER);
+
 		Image cardImage[][] = new Image[2][2];
 		for (int i = 0; i < 2; i++)
 			for (int j = 0; j < 2; j++)
@@ -26,24 +49,25 @@ public class Main extends Application {
 			for (int j = 0; j < 2; j++)
 				imageView[i][j] = new ImageView(cardImage[i][j]);
 
-		Scene scene = new Scene(root, 400, 400);
-		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-		primaryStage.setScene(scene);
-
-		GridPane centerBox = new GridPane();
-		centerBox.setAlignment(Pos.CENTER);
-		
 		for (int i = 0; i < 2; i++)
 			for (int j = 0; j < 2; j++)
 				centerBox.addRow(i, imageView[i][j]);
 
+		//bottomBox
+		HBox bottomBox = new HBox(5);
+		bottomBox.setAlignment(Pos.CENTER);
+		
+		
+		root.setTop(topBox);
+		root.setRight(rightBox);
 		root.setCenter(centerBox);
+		root.setBottom(bottomBox);
+
 		primaryStage.show();
 		primaryStage.setTitle("Memory v0.1");
-		
 
 		imageView[0][0].setOnMouseClicked(event -> {
-			imageView[0][0].setImage(new Image("http://vk.com/images/gifts/256/44.jpg",40,40,true,true));
+			imageView[0][0].setImage(new Image("http://vk.com/images/gifts/256/44.jpg", 40, 40, true, true));
 		});
 
 	}
