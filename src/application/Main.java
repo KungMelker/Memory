60537259c0d365a7a4a8a2868d1c6fe9f2f715ac
JohDav <johan.davidson@yh.nackademin.se>
2015-10-28 14:12:30 +0100

    Changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.effect.Reflection;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
