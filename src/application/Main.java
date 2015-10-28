0cc4fa3b542390b6dead166b74fb097228072995
JohDav <johan.davidson@yh.nackademin.se>
2015-10-28 15:46:18 +0100

    cleanup

--- src/application/Main.java
+++ src/application/Main.java
@@ -74,7 +74,6 @@ public class Main extends Application {
 		pairs_6.setToggleGroup(pairsGroup);
 		pairs_8.setToggleGroup(pairsGroup);
 		pairs_10.setToggleGroup(pairsGroup);
-		pairs_2.setSelected(true);
 
 		leftBox.getChildren().addAll(pairs_2, pairs_4, pairs_6, pairs_8, pairs_10);
 
