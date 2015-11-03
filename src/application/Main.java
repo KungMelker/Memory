7b302b324b94a68156f9a7537c431f34c88da971
JohDav <johan.davidson@yh.nackademin.se>
2015-11-03 10:22:15 +0100

    1000 x 700

--- src/application/Main.java
+++ src/application/Main.java
@@ -32,7 +32,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 
 		BorderPane root = new BorderPane();
-		Scene scene = new Scene(root, 1000, 600);
+		Scene scene = new Scene(root, 1000, 700);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		primaryStage.setScene(scene);
 
