345cc2390b4a3ea322c5bfa93515a04806530015
JohDav <johan.davidson@yh.nackademin.se>
2015-11-03 15:58:13 +0100

    changes in highscore

--- src/application/Main.java
+++ src/application/Main.java
@@ -117,22 +117,27 @@ public class Main extends Application {
 			switch (row_column) {
 			case 2:
 				centerBox = center_2();
+				highpoint.setText(hs.getScore(0));
 				break;
 
 			case 4:
 				centerBox = center_4();
+				highpoint.setText(hs.getScore(1));
 				break;
 
 			case 6:
 				centerBox = center_6();
+				highpoint.setText(hs.getScore(2));
 				break;
 
 			case 8:
 				centerBox = center_8();
+				highpoint.setText(hs.getScore(3));
 				break;
 
 			default:
 				centerBox = center_10();
+				highpoint.setText(hs.getScore(4));
 				break;
 			}
 			centerBox.setAlignment(Pos.CENTER);
@@ -146,7 +151,7 @@ public class Main extends Application {
 		
 		// TODO - add a save function to sQuit - setOnAction
 		sQuit.setOnAction(event -> {
-			hs.score(gameEngine.timePlayed(), gameEngine.getTries(), gameEngine.calculateScore(row_column,gameEngine.timePlayed()));
+			hs.writeFile();
 			primaryStage.close();
 		});
 
