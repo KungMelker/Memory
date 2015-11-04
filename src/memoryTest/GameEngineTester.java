d723f1468d3e9ede84659836bdbb7887b9fa2e5e
Melker Holmgren <melker.holmgren@yh.nackademin.se>
2015-11-04 11:35:57 +0100

    Running some tests

--- src/memoryTest/GameEngineTester.java
+++ src/memoryTest/GameEngineTester.java
@@ -1,14 +1,33 @@
 package memoryTest;
 
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.junit.Test;
+
+import application.GameEngine;
+
+
 //import static org.junit.Assert.*;
 
 //import org.junit.Test;
 
 public class GameEngineTester {
 
-	/*@Test
-	public void test() {
-		fail("Not yet implemented");
-	}*/
-
+	
+	
+	@Test(timeout=10)
+	public void testcalculateScore() {
+		GameEngine ge = new GameEngine();
+		ge.calculateScore(8, 200000);
+	}
+	
+	@Test (expected = NullPointerException.class)
+	public void testNullcalculateScore() {
+		GameEngine ge = new GameEngine();
+		ge.calculateScore(8, 0);
+	}	
+	
 }
