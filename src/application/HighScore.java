4a92984046de4d6b54aa2e1cfa01b986b6cdbf53
Melker Holmgren <melker.holmgren@yh.nackademin.se>
2015-11-04 09:55:54 +0100

    Change code in hi-score

--- src/application/HighScore.java
+++ src/application/HighScore.java
@@ -36,7 +36,7 @@ public class HighScore extends GameEngine {
 	// writes highscore to file
 	public void writeFile() {
 
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathname));) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(pathname,true));) {
 
 			for (int i = 0; i < scoreList.length; i++) {
 				bw.write(scoreList[i]);
